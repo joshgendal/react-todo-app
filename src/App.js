cfcd4c4ab132e050ab628298d1268ba22a46e273
joshcodingdojo <jgendal@codingdojo.com>
2021-07-01 11:50:51 -0600

    Added todo form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
+  const [todoArray, setTodoArray] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO app</h1>
+      <Form setTodoArray={setTodoArray} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Form = (props) => {
+  const { setTodoArray } = props;
+  const [todoText, setTodoText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      text: todoText,
+      completed: false,
+    };
+    setTodoArray((oldArray) => [...oldArray, newTodo]);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="todoText">Todo:</label>
+        <input
+          type="text"
+          name="todoText"
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+      </div>
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default Form;
